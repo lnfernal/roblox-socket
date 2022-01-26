5158ffef3c442bd96c985e13d72d32c2fac0ad29
blocktaco <60831236+blocktaco@users.noreply.github.com>
2022-01-26 07:51:05 -0600

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const PrivatePartyHandler = require('./PrivatePartyHandler.js');
 server.get('/', function (req, res) {
     (async () => {
         const PrivatePartyData = {success: true, id: (await PrivatePartyHandler.CreatePrivatePartyIdentifier()).toString()}
-        res.send(PrivatePartyHandler.CreateNewPrivateParty().Result)
+        res.send(PrivatePartyHandler.CreateNewPrivateParty(10).Result)
     })()
 })
 
