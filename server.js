7e44cfd2d39b858aa2b4853bc4fabf66ed970aa7
blocktaco <60831236+blocktaco@users.noreply.github.com>
2022-01-25 23:57:09 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -4,10 +4,6 @@ const cors = require('cors')
 const app = express()
 .use(cors())
 
-app.listen(port, function(){
-    console.log(`started server at ${port}`)
-})
-
 app.get('/', function (req, res) {
     res.send('GET request has been received.')
     console.log('a')
