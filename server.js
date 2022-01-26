8c523091547783052517f9d0bc4f52d12a0de34f
blocktaco <60831236+blocktaco@users.noreply.github.com>
2022-01-25 22:21:07 -0600

    Add files via upload

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+const port = 8000
+
+app.listen(port, function(){
+    console.log(`started server at ${port}`)
+})
+
 app.get('/', function (req, res) {
     res.send('GET request has been received.')
 })
