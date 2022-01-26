68e1fb31b0b283d824e135f801cc7c10fc24db83
blocktaco <60831236+blocktaco@users.noreply.github.com>
2022-01-26 07:52:11 -0600

    Add files via upload

--- PrivatePartyHandler.js
+++ PrivatePartyHandler.js
@@ -23,12 +23,12 @@ async function GetPrivatePartyFromUserIdAsync(UserId) {
 
 async function CreateNewPrivateParty(userid) {
     return (async () => {
-        const PartyID = await CreatePrivatePartyIdentifier()
+        /*const PartyID = await CreatePrivatePartyIdentifier()
         PrivatePartyData.set(PartyID, {
             PartyID: Number(PartyID),
             Owner: userid,
             Players: []
-        })
+        })*/
 
         return {success: true, id: PartyID}
     })()
